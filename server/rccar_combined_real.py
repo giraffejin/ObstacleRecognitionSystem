88f46198630bff15420c2015e94c1f39ca321c0f
giraffejin <35915139+giraffejin@users.noreply.github.com>
2021-04-10 20:46:19 +0900

    장애물 인식 시스템(서버)

--- server/rccar_combined_real.py
+++ server/rccar_combined_real.py
@@ -28,20 +28,14 @@ print('connected\n')
 
 def trafficlight(video_path):
   Tvideo = cv2.VideoCapture(video_path)
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
   PATH_TO_FROZEN_GRAPH = os.path.join('tf_graph_rcnn', 'rcnn_v2.pb')
-
-  # List of the strings that is used to add correct label for each box.
   PATH_TO_LABELS = os.path.join('legacy/training', 'labelmap.pbtxt')
-
   NUM_CLASSES=2
 
   #load model
   label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
   categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
   category_index = label_map_util.create_category_index(categories)
-
-
 
   detection_graph = tf.Graph()
   with detection_graph.as_default():
@@ -52,8 +46,6 @@ def trafficlight(video_path):
       tf.import_graph_def(od_graph_def, name='')
 
     sess=tf.Session(graph=detection_graph)
-
-
 
   image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
   detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
@@ -69,6 +61,7 @@ def trafficlight(video_path):
 
   #v_result: final result for 5 seconds video
   #result_flag: result for each frame
+	
   while(Tvideo.isOpened()):
     ret, frame = Tvideo.read()
     frame = cv2.resize(frame, dsize=(500,500), interpolation = cv2.INTER_AREA)
@@ -133,6 +126,7 @@ def trafficlight(video_path):
     else:
       result_s = signal_status_string(result_c,result_flag, k, result_s)
 
+#block detection
 def yellowblock(video_path):
   Bvideo = cv2.VideoCapture(video_path)
 
